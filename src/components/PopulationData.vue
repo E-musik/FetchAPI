1808f8e0c75b80802dc0e24d83c4b585f289d22f
E-musik <emmamusik888@gmail.com>
2023-07-26 05:59:20 +0100

    FetchAPI

--- src/components/PopulationData.vue
+++ src/components/PopulationData.vue
@@ -1,11 +1,17 @@
 <template>
     <div id="App">
       <h1>Population Data</h1>
+      
+    <button class="get" @click="data.push([])">Get Data</button>
+    <button @click="data.pop ()">Remove Data</button>
+
+
       <ul>
         <li v-for="i in data" :key="i.ID">
           {{ i.country }}: {{ i.Population }}
         </li>
       </ul>
+      
     </div>
   </template>
   
